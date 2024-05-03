0f9dd42409652a4baabbede26e2ac2826e2d9888
23730117-NguyenThuyNghiQuan <23730117@ms.uit.edu.vn>
2024-05-03 23:21:54 +0700

    Add handler.h

---
+++ src/handler.h
@@ -0,0 +1,17 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include "snake.h"
+
+void thaoTacPhimR(Snake *r, int &huong);
+
+void thaoTacPhimM(Snake *r, int &huong);
+
+void thaoTacPhimEsc();
+
+void khiConRanPhamQuy(Snake *r);
+
+void tamDung(Snake *r, int &huong);
+
+
+#endif //HANDLER_H
