1d0474023e32cc77fd045adc97950b39b9dc8298
Huy Huynh <huyhvq@icloud.com>
2024-05-03 11:08:32 +0700

    add ui_screen.cpp and ui_screen.h

---
+++ src/ui_screen.h
@@ -0,0 +1,12 @@
+#ifndef UI_SCREEN_H
+#define UI_SCREEN_H
+#include <string>
+
+using namespace std;
+
+void initialScreenSize();
+void anConTro();
+void veGiaoDien();
+void veMenu();
+
+#endif //UI_SCREEN_H
