55431245906951e962505921b704f090dfa3badb
Huy Huynh <huyhvq@icloud.com>
2024-05-04 21:27:32 +0700

    integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+#include "ui_screen.h"
+#include "snake.h"
+#include "food.h"
+#include "config.h"
+#include "handler.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Snake r;
+    int huong = 0;
+    int t;
+    SetConsoleOutputCP(65001);
+    // Ẩn con trỏ
+    anConTro();
+    // Set màu
+    system("color 02");
+    veGiaoDien();
+    r.tocDo = doKho;
+    veConMoi(r);
+    r.veThanRan();
+    while (true) {
+        r.veDauRan();
+        // Kiểm tra nếu rắn đụng biên hay cắn thân
+        if (r.ktConRanPhamQuy()) {
+            khiConRanPhamQuy(&r);
+        }
+        Sleep(r.tocDo);
+        if (kbhit()) {
+            t = getch();
+            if (t == 224) {
+                char c = getch();
+                if (c == 75 && huong != 0) huong = 2;
+                else if (c == 72 && huong != 1) huong = 3;
+                else if (c == 77 && huong != 2) huong = 0;
+                else if (c == 80 && huong != 3) huong = 1;
+            }
+            if ((t == 'a' || t == 'A') && huong != 0) huong = 2;
+            else if ((t == 'w' || t == 'W') && huong != 1) huong = 3;
+            else if ((t == 'd' || t == 'D') && huong != 2) huong = 0;
+            else if ((t == 's' || t == 'S') && huong != 3) huong = 1;
+            else if (t == ' ') // Dừng game
+                tamDung(&r, huong);
+            else if (t == 'r' || t == 'R')
+                thaoTacPhimR(&r, huong);
+            else if (t == 'm' || t == 'M')
+                thaoTacPhimM(&r, huong);
+            else if (t == 27)
+                //thaoTacPhimEsc(); Đang trong game ta về menu chinh
+                    thaoTacPhimM(&r, huong);
+        }
+        if (r.ktDungConMoi()) {
+            r.anConMoi();
+            veConMoi(r);
+            capNhatDiem(diem);
+            if (r.tocDo >= 100 && diem > 0 && diem % 50 == 0)
+                r.tocDo -= 20;
+        }
+        r.diChuyen(huong);
+    }
     return 0;
 }
--- src/snake.cpp
+++ src/snake.cpp
@@ -16,7 +16,7 @@ Snake::Snake() {
 
 void Snake::khoiTaoThanRan() {
     Point start = layDiemTrungTam();
-    for (int i = 0; i < doDai; i++)
+    for(int i = 0; i < doDai; i++)
     {
         dotRan[i].cot = start.cot--;
         dotRan[i].dong = start.dong;
@@ -34,42 +34,42 @@ void Snake::diChuyen(int Huong)
         lonLen = false;
 
     // vị trí đầu rẵn sẽ là vị trí thân rắn khi di chuyên
-    diChuyenCotDong(dotRan[0].cot, dotRan[0].dong);
+    diChuyenCotDong(dotRan[0].cot,dotRan[0].dong);
     std::cout << "+";
 
-    for (int i = doDai - 1; i > 0; i--)
-        dotRan[i] = dotRan[i - 1];
-    if (Huong == 0) dotRan[0].cot = dotRan[0].cot + 1;// rẽ phải
-    if (Huong == 1) dotRan[0].dong = dotRan[0].dong + 1;// đi xuống
-    if (Huong == 2) dotRan[0].cot = dotRan[0].cot - 1;// rẽ trái
-    if (Huong == 3) dotRan[0].dong = dotRan[0].dong - 1;// đi lên
+    for (int i = doDai-1; i>0; i--)
+        dotRan[i] = dotRan[i-1];
+    if (Huong==0) dotRan[0].cot = dotRan[0].cot + 1;// rẽ phải
+    if (Huong==1) dotRan[0].dong = dotRan[0].dong + 1;// đi xuống
+    if (Huong==2) dotRan[0].cot = dotRan[0].cot - 1;// rẽ trái
+    if (Huong==3) dotRan[0].dong = dotRan[0].dong - 1;// đi lên
 
     // Điều chỉnh tọa độ nếu rắn đi qua tường
-    if (choPhepQuaTuong)
+    if(choPhepQuaTuong)
         ranDiChuyenQuaTuong();
 }
 
 void Snake::ranDiChuyenQuaTuong()
 {
     // Kiểm tra xem đầu rắn có ra khỏi màn hình không
-    if (dotRan[0].cot == gocTraiTren.cot)
+    if (dotRan[0].cot == gocTraiTren.cot )
         dotRan[0].cot = gocPhaiDuoi.cot - 1;
-    else if (dotRan[0].cot == gocPhaiDuoi.cot)
+    else if (dotRan[0].cot == gocPhaiDuoi.cot )
         dotRan[0].cot = gocTraiTren.cot + 1;
-    else if (dotRan[0].dong == gocTraiTren.dong)
+    else if (dotRan[0].dong == gocTraiTren.dong )
         dotRan[0].dong = gocPhaiDuoi.dong - 1;
-    else if (dotRan[0].dong == gocPhaiDuoi.dong)
+    else if (dotRan[0].dong == gocPhaiDuoi.dong )
         dotRan[0].dong = gocTraiTren.dong + 1;
 }
 
 void Snake::veConRan()
 {
-    diChuyenCotDong(dotRan[0].cot, dotRan[0].dong);
+    diChuyenCotDong(dotRan[0].cot,dotRan[0].dong);
     std::cout << "O";
     for (int i = 1; i < doDai; i++)
     {
-        diChuyenCotDong(dotRan[i].cot, dotRan[i].dong);
-        std::cout << "+";
+        diChuyenCotDong(dotRan[i].cot,dotRan[i].dong);
+        std::cout<<"+";
     }
 }
 
@@ -77,29 +77,29 @@ void Snake::veThanRan()
 {
     for (int i = 1; i < doDai; i++)
     {
-        diChuyenCotDong(dotRan[i].cot, dotRan[i].dong);
-        std::cout << "+";
+        diChuyenCotDong(dotRan[i].cot,dotRan[i].dong);
+        std::cout<<"+";
     }
 }
 
 void Snake::veDauRan()
 {
-    diChuyenCotDong(dotRan[0].cot, dotRan[0].dong);
+    diChuyenCotDong(dotRan[0].cot,dotRan[0].dong);
     std::cout << "O";
 }
 
 void Snake::xoaDotCuoi()
 {
-    diChuyenCotDong(dotRan[doDai - 1].cot, dotRan[doDai - 1].dong);
-    std::cout << " ";
+    diChuyenCotDong(dotRan[doDai-1].cot, dotRan[doDai-1].dong);
+    std::cout<< " ";
 }
 
 void Snake::xoaConRan()
 {
     for (int i = 0; i < doDai; i++)
     {
-        diChuyenCotDong(dotRan[i].cot, dotRan[i].dong);
-        std::cout << " ";
+        diChuyenCotDong(dotRan[i].cot,dotRan[i].dong);
+        std::cout<<" ";
     }
 }
 
@@ -109,11 +109,11 @@ void Snake::veConRanChet()
     for (int i = 1; i < doDai; i++)
     {
         diChuyenCotDong(dotRan[i].cot, dotRan[i].dong);
-        std::cout << ".";
+        std::cout<< ".";
     }
     // Ve cai dau khi chet
     diChuyenCotDong(dotRan[0].cot, dotRan[0].dong);
-    std::cout << "*";
+    std::cout<< "*";
 }
 
 bool Snake::ktConRanPhamQuy()
@@ -123,7 +123,7 @@ bool Snake::ktConRanPhamQuy()
     // Đầu rắn đốt[0] trùng với thân rắn từ đốt[1] đến đốt độ dài con rắn -1
     for (int i = 1; i < doDai; i++)
     {
-        if (dotRan[0].cot == dotRan[i].cot && dotRan[0].dong == dotRan[i].dong)
+        if(dotRan[0].cot == dotRan[i].cot && dotRan[0].dong == dotRan[i].dong)
             return true;
     }
     return false;
@@ -136,14 +136,14 @@ bool Snake::ktConRanChamKhung()
         return false;
     }
     // Khi dau ran trùng -> Cot trai || cot phai || dong tren || dong duoi
-    if (dotRan[0].cot == gocTraiTren.cot || dotRan[0].cot == gocPhaiDuoi.cot || dotRan[0].dong == gocTraiTren.dong || dotRan[0].dong == gocPhaiDuoi.dong)
+    if(dotRan[0].cot == gocTraiTren.cot || dotRan[0].cot == gocPhaiDuoi.cot || dotRan[0].dong == gocTraiTren.dong || dotRan[0].dong == gocPhaiDuoi.dong)
         return true;
     return false;
 }
 
 void Snake::anConMoi()
 {
-    dotRan[doDai] = dotRan[doDai - 1];
+    dotRan[doDai] = dotRan[doDai-1];
     doDai += 1;
     diem += 10;
     lonLen = true;
