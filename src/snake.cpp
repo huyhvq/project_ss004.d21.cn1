38c996a511a910796150c008de8762c51abd3635
Huy Huynh <huyhvq@icloud.com>
2024-05-04 11:03:14 +0700

    fix snake class

--- src/snake.cpp
+++ src/snake.cpp
@@ -22,3 +22,44 @@ void Snake::khoiTaoThanRan() {
         dotRan[i].dong = start.dong;
     }
 }
+
+void Snake::xoaConRan()
+{
+    for (int i = 0; i < doDai; i++)
+    {
+        diChuyenCotDong(dotRan[i].cot,dotRan[i].dong);
+        std::cout<<" ";
+    }
+}
+
+void Snake::veThanRan()
+{
+    for (int i = 1; i < doDai; i++)
+    {
+        diChuyenCotDong(dotRan[i].cot,dotRan[i].dong);
+        std::cout<<"+";
+    }
+}
+
+void Snake::veConRanChet()
+{
+    // Ve minh khi chet
+    for (int i = 1; i < doDai; i++)
+    {
+        diChuyenCotDong(dotRan[i].cot, dotRan[i].dong);
+        std::cout<< ".";
+    }
+    // Ve cai dau khi chet
+    diChuyenCotDong(dotRan[0].cot, dotRan[0].dong);
+    std::cout<< "*";
+}
+
+void Snake::lamMoiRan() {
+    xoaConRan();
+    Point snakeHead = dotRan[0];
+    veLaiDiemVienBiXoa(snakeHead);
+    doDai = doDaiMacDinh;
+    tocDo = doKho;
+    khoiTaoThanRan();
+    capNhatDiem(0);
+}
